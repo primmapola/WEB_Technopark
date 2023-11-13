7bc6137fd7a7276c6dc57d4dd9a6ad527d42b40b
dongrigory <dongrigory29@gmail.com>
2023-11-13 16:38:51 +0300

    Update views function

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    questions = [
+        {
+            'id': index,
+            'title': f'Question {index}',
+            'content': f'Lorem ipsum? {index}'
+        } for i in range(5)
+    ]
+    return render(request, template_name='index.html', context={'questions': questions})
+
+def question(request):
+    return render(request, template_name='question.html')
