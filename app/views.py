63a76ecf676f9dce956efff9ed63c18ad70bd4c0
dongrigory <dongrigory29@gmail.com>
2023-11-15 02:36:27 +0300

    hw2🤡

--- app/views.py
+++ app/views.py
@@ -4,34 +4,49 @@ from django.core.paginator import Paginator
 
 questions = [
         {
-            'id': i,
-            'title': f'Question {i}',
-            'content': f'Lorem ipsum? {i}'
+            'id': i+1,
+            'title': f'Question {i+1}',
+            'content': f'Lorem ipsum? {i+1}'
         } for i in range(15)
     ]
 
 
-
-# Assuming 'questions' is defined as before
-
-# Create your views here.
 def index(request):
-    # Define the number of items per page
     items_per_page = 3
 
-    # Create a Paginator object
     paginator = Paginator(questions, items_per_page)
 
-    # Get the page number from the request
     page_number = request.GET.get('page')
 
-    # Get the desired page
     page_obj = paginator.get_page(page_number)
 
-    # Render the page, passing the page object
     return render(request, 'index.html', {'page_obj': page_obj})
 
 def question(request, question_id):
-    item = questions[question_id]
+    item = questions[question_id-1]
     return render(request, template_name='question.html', context={'question': item})
 
+def ask(request):
+    return render(request, template_name='ask.html')
+
+from django.shortcuts import render
+
+def tagged_questions(request, tag_name):
+    items_per_page = 3
+
+    paginator = Paginator(questions, items_per_page)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'tagged_questions.html', {'page_obj': page_obj, 'tag_name': tag_name})
+
+def settings(request):
+    return render(request, template_name='settings.html')
+
+def login(request):
+    return render(request, template_name='login.html', context={'error_message': True})
+
+def signup(request):
+    return render(request, template_name='signup.html', context={'error_message': True})
--- webDZ/urls.py
+++ webDZ/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from app import views
+
 urlpatterns = [
-    path('', views.index, name = 'mainView'),
-    path('question/<int:question_id>', views.question, name = 'questionView'),
+    path('', views.index, name='mainView'),
+    path('question/<int:question_id>', views.question, name='questionView'),
+    path('ask', views.ask, name='askView'),
+    path('tagged/<str:tag_name>/', views.tagged_questions, name='tagged_questions'),
+    path('settings', views.settings, name='settingsView'),
+    path('login', views.login, name='loginView'),
+    path('signup', views.signup, name='signupView'),
     path('admin/', admin.site.urls),
 ]
